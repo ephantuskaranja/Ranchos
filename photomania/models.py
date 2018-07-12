5864128f04842e6a2ff479b78544ee1b9efb6e19
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-12 11:18:49 +0300

    create db models and migrate

--- photomania/models.py
+++ photomania/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =50)
+    description = models.TextField()
+
+
+class Location(models.Model):
+    location = models.DecimalField(max_digits=9, decimal_places=6)
+    image = models.ForeignKey(Image)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length = 50)
+    image = models.ForeignKey(Image)
--- photomania/urls.py
+++ photomania/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^', views.welcome, name = 'welcome' )
+    url(r'^$', views.welcome, name = 'welcome' )
 ]
