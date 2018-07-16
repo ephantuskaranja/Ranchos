860160227c5b027e49d5879db8bc7ac7158b8986
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-16 11:29:13 +0300

    implement search images by category

--- photomania/models.py
+++ photomania/models.py
@@ -4,13 +4,21 @@ from django.db import models
 
 class Location(models.Model):
     location = models.CharField(max_length = 50)
+
     def __str__(self):
         return self.location
+        class Meta:
+            ordering = ['location']
+
+
 
 class Category(models.Model):
     category = models.CharField(max_length = 50)
+
     def __str__(self):
         return self.category
+        class Meta:
+            ordering = ['category']
 
 
 class Image(models.Model):
@@ -20,14 +28,20 @@ class Image(models.Model):
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
 
+    @classmethod
+    def get_images(cls):
+       images = cls.objects.all()
+       return images
 
     def __str__(self):
         return self.name
+        class Meta:
+            ordering = ['name']
 
     def save_image(self):
         self.save()
 
     @classmethod
-    def get_images(cls):
-        images = cls.objects.all()
-        return images
+    def searched_categories(cls, category):
+        category = cls.objects.filter(description__icontains=category)
+        return category
--- photomania/views.py
+++ photomania/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Image
+from django.db.models import Q
 
 # Create your views here.
 def welcome(request):
     images = Image.get_images()
     return render(request, 'photomania/home.html',{"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_categories = Image.searched_categories(category)
+        message = f"{category}"
+
+        return render(request, 'photomania/search.html',{"message":message,"categories": searched_categories})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photomania/search.html',{"message":message})
